75ab20ddcc0651243fd74c270779b1c1342f04a9
Jose Medrano <jose.medrano@microsoft.com>
2018-03-01 12:52:09 +0100

    Changes the order of the context menu and some labels

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui.Pads/ErrorListPad.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui.Pads/ErrorListPad.cs
@@ -378,19 +378,15 @@ namespace MonoDevelop.Ide.Gui.Pads
 		{
 			var menu = new ContextMenu ();
 
-			var help = new ContextMenuItem (GettextCatalog.GetString ("Show Error Reference"));
+			var help = new ContextMenuItem (GettextCatalog.GetString ("Go to Reference"));
 			help.Clicked += OnShowReference;
 			menu.Add (help);
-
-			var copy = new ContextMenuItem (GettextCatalog.GetString ("_Copy"));
-			copy.Clicked += OnTaskCopied;
-			menu.Add (copy);
 
 			var goBuild = new ContextMenuItem (GettextCatalog.GetString ("Go to Log"));
 			goBuild.Clicked += async (s, e) => await OnGoToLog (s, e);
 			menu.Add (goBuild);
 
-			var jump = new ContextMenuItem (GettextCatalog.GetString ("_Go to Task"));
+			var jump = new ContextMenuItem (GettextCatalog.GetString ("Go to Task"));
 			jump.Clicked += OnTaskJumpto;
 			menu.Add (jump);
 
@@ -398,6 +394,15 @@ namespace MonoDevelop.Ide.Gui.Pads
 			                                          restoreID,
 			                                          GettextCatalog.GetString ("Type"),
 			                                          GettextCatalog.GetString ("Validity"));
+
+			menu.Add (new SeparatorContextMenuItem ());
+
+			var copy = new ContextMenuItem (GettextCatalog.GetString ("_Copy"));
+			copy.Clicked += OnTaskCopied;
+			menu.Add (copy);
+
+
+			menu.Add (new SeparatorContextMenuItem ());
 
 			var columns = new ContextMenuItem (GettextCatalog.GetString ("Columns"));
 			columns.SubMenu = columnsMenu;
